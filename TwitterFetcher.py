581bb9f92fad8edc3f2b44c999f9ef5a845e4081
armando <armandowb95@gmail.com>
2022-01-19 16:00:53 +0200

    first python file sentiment analysis

---
+++ TwitterFetcher.py
@@ -0,0 +1,98 @@
+# Import Libraries
+from textblob import TextBlob
+import sys
+import tweepy
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import os
+import nltk
+import pycountry
+import re
+import string
+from wordcloud import WordCloud, STOPWORDS
+from PIL import Image
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from langdetect import detect
+from nltk.stem import SnowballStemmer
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Authentication
+consumerKey = “Type your consumer key here”
+consumerSecret = “Type your consumer secret here”
+accessToken = “Type your accedd token here”
+accessTokenSecret = “Type your access token secret here”
+auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
+auth.set_access_token(accessToken, accessTokenSecret)
+api = tweepy.API(auth)
+
+
+# Sentiment Analysis
+def percentage(part, whole):
+    return 100 * float(part) / float(whole)
+
+
+keyword = input(“Please
+enter
+keyword or hashtag
+to
+search: “)
+noOfTweet = int(input(“Please
+enter
+how
+many
+tweets
+to
+analyze: “))
+tweets = tweepy.Cursor(api.search, q=keyword).items(noOfTweet)
+positive = 0
+negative = 0
+neutral = 0
+polarity = 0
+tweet_list = []
+neutral_list = []
+negative_list = []
+positive_list = []
+for tweet in tweets:
+
+    # print(tweet.text)
+    tweet_list.append(tweet.text)
+    analysis = TextBlob(tweet.text)
+    score = SentimentIntensityAnalyzer().polarity_scores(tweet.text)
+    neg = score[‘neg’]
+    neu = score[‘neu’]
+    pos = score[‘pos’]
+    comp = score[‘compound’]
+    polarity += analysis.sentiment.polarity
+
+    if neg > pos:
+        negative_list.append(tweet.text)
+    negative += 1
+elif pos > neg:
+positive_list.append(tweet.text)
+positive += 1
+
+elif pos == neg:
+neutral_list.append(tweet.text)
+neutral += 1
+positive = percentage(positive, noOfTweet)
+negative = percentage(negative, noOfTweet)
+neutral = percentage(neutral, noOfTweet)
+polarity = percentage(polarity, noOfTweet)
+positive = format(positive, ‘.1
+f’)
+negative = format(negative, ‘.1
+f’)
+neutral = format(neutral, ‘.1
+f’)
+
+#Number of Tweets (Total, Positive, Negative, Neutral)
+tweet_list = pd.DataFrame(tweet_list)
+neutral_list = pd.DataFrame(neutral_list)
+negative_list = pd.DataFrame(negative_list)
+positive_list = pd.DataFrame(positive_list)
+print(“total number: “,len(tweet_list))
+print(“positive number: “,len(positive_list))
+print(“negative number: “, len(negative_list))
+print(“neutral number: “,len(neutral_list))
