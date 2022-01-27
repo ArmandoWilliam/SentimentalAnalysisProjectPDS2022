822f29267460bbcaac171407bd413bfefe5545c2
OlgaGeromichalou <olga28898@hotmail.com>
2022-01-27 06:42:46 +0200

    authentication problem

--- TwitterFetcher.py
+++ TwitterFetcher.py
@@ -1,74 +1,19 @@
 from textblob import TextBlob
-import sys
 import tweepy
-import matplotlib.pyplot as plt
+from tweepy import OAuthHandler
 import pandas as pd
-import numpy as np
-import os
-import nltk
-import pycountry
-import re
-import string
-
-from PIL import Image
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from langdetect import detect
-from nltk.stem import SnowballStemmer
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from sklearn.feature_extraction.text import CountVectorizer
 
 # Authentication
-consumerKey = 'dN0g9U8BVlURKs65o8rP5mnhE'
-consumerSecret = 'pnsnUCnbDd1M1BIHg45IMi3IPbjaTkDXu6HLq9XNpiCB7fHiq6'
-accessToken = '1482383090274254864-8Hi0J2Bu4yBJcqUeJ2NjLSEd5ufAy1'
-accessTokenSecret = 'aoUMItsal3nRxd2eDoI725SS7E4z1kOdMgK3XyQ1p24m8'
+consumerKey = 'flKUc2Kf4UCMgWwSY59tjQxxp'
+consumerSecret = 'Fkkd7jA612jNOf6nva0qealFZsYfP06GR8LoeratMEDPZHWxra'
+accessToken = '1482383090274254864-O1c4tZmJrEzquTb8tcivg64c1MjUjy'
+accessTokenSecret = '1axxDUDeLegFFep1077MH4nGTPfhHqIBz6sPD3gG6ajCr'
 auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
 auth.set_access_token(accessToken, accessTokenSecret)
 api = tweepy.API(auth)
 
-
-# Sentiment Analysis
-def percentage(part, whole):
-    return 100 * float(part) / float(whole)
-
-
-keyword = 'Novak Djokovic'
-noOfTweet = 200
-tweets = tweepy.Cursor(api.search_tweets, q=keyword).items(noOfTweet)
-positive = 0
-negative = 0
-neutral = 0
-polarity = 0
-tweet_list = []
-neutral_list = []
-negative_list = []
-positive_list = []
-for tweet in tweets:
-
-    # print(tweet.text)
-    tweet_list.append(tweet.text)
-    analysis = TextBlob(tweet.text)
-    score = SentimentIntensityAnalyzer().polarity_scores(tweet.text)
-    neg = score['neg']
-    neu = score['neu']
-    pos = score['pos']
-    comp = score['compound']
-    polarity += analysis.sentiment.polarity
-
-    if neg > pos:
-        negative_list.append(tweet.text)
-        negative += 1
-    elif pos > neg:
-        positive_list.append(tweet.text)
-        positive += 1
-
-    elif pos == neg:
-         neutral_list.append(tweet.text)
-         neutral += 1
-positive = percentage(positive, noOfTweet)
-negative = percentage(negative, noOfTweet)
-neutral = percentage(neutral, noOfTweet)
-polarity = percentage(polarity, noOfTweet)
-positive = format(positive, '.1f')
-negative = format(negative, '.1f')
-neutral = format(neutral, '.1f')
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
