2a87f041077135c0c1bb175a4b807464168d1c2a
OlgaGeromichalou <olga28898@hotmail.com>
2022-01-30 14:19:43 +0200

    code without keys

--- TwitterFetcher.py
+++ TwitterFetcher.py
@@ -2,18 +2,62 @@ from textblob import TextBlob
 import tweepy
 from tweepy import OAuthHandler
 import pandas as pd
+from textblob import TextBlob
+import re
 
 # Authentication
-consumerKey = 'flKUc2Kf4UCMgWwSY59tjQxxp'
-consumerSecret = 'Fkkd7jA612jNOf6nva0qealFZsYfP06GR8LoeratMEDPZHWxra'
-accessToken = '1482383090274254864-O1c4tZmJrEzquTb8tcivg64c1MjUjy'
-accessTokenSecret = '1axxDUDeLegFFep1077MH4nGTPfhHqIBz6sPD3gG6ajCr'
+consumerKey = ''
+consumerSecret = ''
+accessToken = ''
+accessTokenSecret = ''
 auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
 auth.set_access_token(accessToken, accessTokenSecret)
 api = tweepy.API(auth)
 
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-except:
-    print("Error during authentication")
+
+def clean_tweet(tweet):
+    return ' '.join(re.sub(r"(@[A-Za-z0-9_]+)|(#[A-Za-z0-9_]+)|([^0-9A-Za-z \t])|(http\S+)|(www.\S+)", " ", tweet))
+
+def get_tweet_sentiment(tweet):
+    # create TextBlob object of passed tweet text
+    analysis = TextBlob(clean_tweet(tweet))
+    # set sentiment
+    if analysis.sentiment.polarity > 0:
+        return 'positive'
+    elif analysis.sentiment.polarity == 0:
+        return 'neutral'
+    else:
+        return 'negative'
+
+fetch_tweets = []
+players = ['Nadal','Tsitsipas','Djokovic']
+limit=1000
+for player in players :
+    tweets = tweepy.Cursor(api.search_tweets, q=player, count=100, tweet_mode='extended').items(limit)
+
+    for tweet in tweets:
+        # find the hashtags in the tweet
+        hstgs = []
+        for hstg in tweet.entities['hashtags']:
+            hstgs.append(hstg['text'])
+
+        tweet_dict = {'searchParam': player,
+            'user_name': tweet.user.screen_name,
+            'text': tweet.full_text,
+            'hashtags': hstgs,
+            'date': tweet.created_at,
+            'location': tweet.user.location,
+            'number_of_followers':tweet.user.followers_count,
+            'number_of tweets':tweet.user.statuses_count,
+            'sentiment':get_tweet_sentiment(tweet.full_text)
+            }
+        if tweet.retweet_count > 0:
+            # if tweet has retweets, ensure that it is appended only once
+            if tweet_dict not in tweets:
+                fetch_tweets.append(tweet_dict)
+        else:
+            fetch_tweets.append(tweet_dict)
+
+
+df_tweets = pd.DataFrame(fetch_tweets)
+print(df_tweets)
